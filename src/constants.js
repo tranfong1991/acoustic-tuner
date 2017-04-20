e656c3b5747c7c4128408da7e330c39db10931f4
Andy Tran <tran_fong1991@yahoo.com>
2017-04-19 23:12:45 -0500

    working skill

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,14 @@
 'use strict';
 
 module.exports = Object.freeze({
-	appId: 'amzn1.ask.skill.1937d62b-b63b-4a7d-974b-ffea2d078066'
+	appId: 'amzn1.ask.skill.1937d62b-b63b-4a7d-974b-ffea2d078066',
+
+	audios: {
+		"low-e": "https://s3.amazonaws.com/alexa-tune/tunes/low-E.mp3",
+		"a": "https://s3.amazonaws.com/alexa-tune/tunes/A.mp3",
+		"d": "https://s3.amazonaws.com/alexa-tune/tunes/D.mp3",
+		"g": "https://s3.amazonaws.com/alexa-tune/tunes/G.mp3",
+		"b": "https://s3.amazonaws.com/alexa-tune/tunes/B.mp3",
+		"high-e": "https://s3.amazonaws.com/alexa-tune/tunes/high-E.mp3"
+	}
 });
--- src/handlers.js
+++ src/handlers.js
@@ -1,31 +1,52 @@
 'use strict';
 
 var Alexa = require('alexa-sdk');
+var constants = require('./constants');
+var instructionSpeech = "To use this skill, you can say something like, " + 
+	"play gee, or, play dee 3 times. What do you want to play?";
 
 var handlers = {
 	"LaunchRequest": function(){
-		this.emit(':ask', "Welcome to Tune. Please say a note name with its optional frequency.",
-			"Please say a note name with its optional frequency");
+		this.emit(':ask', "Welcome to Acoustic. " + instructionSpeech,
+			"Please say a note you want to play.");
 	},
 	"PlayNoteIntent": function(){
 		var note = this.event.request.intent.slots.Note.value;
+		var audioUrl = getAudioUrl(trimDot(note));
 
-		this.emit(':tell', "Playing note " + note);
+		if(audioUrl == null)
+			this.emit('Unhandled');
+		else this.emit(':ask', "<audio src = '" + audioUrl + "'/> What do you want to play next?");
 	},
 	"PlayNoteWithFrequencyIntent": function(){
 		var note = this.event.request.intent.slots.Note.value;
 		var frequency = parseInt(this.event.request.intent.slots.Frequency.value);
 
-		this.emit(':tell', "Playing note " + note + " " + frequency + " times");
+		if(frequency < 1 || frequency > 5){
+			this.emit(':ask', "Sorry, frequency should be between 1 and 5. Please try again.");
+			return;
+		}
+
+		var audioUrl = getAudioUrl(trimDot(note));
+		if(audioUrl == null){
+			this.emit('Unhandled');
+			return;
+		}
+
+		var audioTag = "";
+		for(var i = 0; i < frequency; i++){
+			audioTag += "<audio src = '" + audioUrl + "'/>";
+		}
+		this.emit(':ask', audioTag + " What do you want to play next?");
 	},
 	"AMAZON.HelpIntent": function(){
-		this.emit(':tell', "To use this skill, please say a note name with its optional frequency.");
+		this.emit(':ask', instructionSpeech);
 	},
 	"AMAZON.StopIntent": function(){
-		this.emit(':tell', "Thank you for using Tune");
+		this.emit('AMAZON.CancelIntent');
 	},
 	"AMAZON.CancelIntent": function(){
-		this.emit(':tell', "Thank you for using Tune");
+		this.emit(':tell', "Thank you for using Acoustic");
 	},
 	"Unhandled": function(){
 		var speech = "Sorry, I could not understand. Please say a note you want to play.";
@@ -33,5 +54,40 @@ var handlers = {
 		this.emit(':responseReady');
 	}
 };
+
+function trimDot(note){
+	return note.split('.').join("");
+}
+
+function getAudioUrl(note){
+	var url = null;
+	switch(note.toLowerCase()){
+		case 'low ee':
+		case 'low e':
+			url = constants.audios['low-e'];
+			break;
+		case 'ay':
+		case 'a':
+			url = constants.audios['a'];
+			break;
+		case 'dee':
+		case 'd':
+			url = constants.audios['d'];
+			break;
+		case 'gee':
+		case 'g':
+			url = constants.audios['g'];
+			break;
+		case 'bee':
+		case 'b':
+			url = constants.audios['b'];
+			break;
+		case 'high ee':
+		case 'high e':
+			url = constants.audios['high-e'];
+			break;
+	}
+	return url;
+}
 
 module.exports = handlers;
